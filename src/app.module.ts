3664ee2496c5044de6e74fd3b977e3a79fc24765
Ghost of Spectre <ghostofspectre@Ghosts-MacBook-Pro.local>
2023-05-09 20:57:16 -0500

    added new new modules to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { OrdersModule } from './orders/orders.module';
+import { StripeModule } from './integrations/stripe/stripe.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [OrdersModule],
+  imports: [AuthModule, OrdersModule, StripeModule, ProductsModule, UsersModule, ScheduleModule.forRoot()],
   controllers: [],
   providers: [],
 })
