a38e0d245b2a1046fcfb1066a50121e99f0e041f
flippedcoder <milecia.mcgregor@aol.com>
2023-08-23 16:45:17 -0500

    added dto for create order validation

--- src/orders/orders.interface.ts
+++ src/orders/orders.interface.ts
@@ -1,7 +1,17 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 export interface Order {
   id: number;
   name: string;
   total: number;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export class CreateOrderDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  total: number;
 }
